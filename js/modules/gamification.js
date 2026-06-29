c372aad21a3d0d8b8d84c73f4fcd0fc9c34bb969
julioanpo-m <julio.anpo@gmail.com>
2026-06-28 18:36:16 -0600

    Update gamification.js

--- js/modules/gamification.js
+++ js/modules/gamification.js
@@ -1,31 +1,13 @@
-// js/modules/gamification.js
 import { SAB_CONSTANTS } from '../core/constants.js';
 
 export const GamificationEngine = {
     calcularEscalaTitanes: function(tonelajeTotalKg) {
         const titanes = SAB_CONSTANTS.TITANES;
-        let nivelAlcanzado = "Ninguno";
-        let comparativaReal = "";
-
-        if (tonelajeTotalKg >= titanes.NIVEL_5.peso_kg) {
-            nivelAlcanzado = titanes.NIVEL_5.nombre;
-            comparativaReal = `${(tonelajeTotalKg / titanes.NIVEL_5.peso_kg).toFixed(1)} Ballenas Azules`;
-        } else if (tonelajeTotalKg >= titanes.NIVEL_4.peso_kg) {
-            nivelAlcanzado = titanes.NIVEL_4.nombre;
-            comparativaReal = `${(tonelajeTotalKg / titanes.NIVEL_4.peso_kg).toFixed(1)} Elefantes Africanos`;
-        } else if (tonelajeTotalKg >= titanes.NIVEL_3.peso_kg) {
-            nivelAlcanzado = titanes.NIVEL_3.nombre;
-            comparativaReal = `${(tonelajeTotalKg / SAB_CONSTANTS.REALIDAD.IGNIS_4CYL_KG).toFixed(1)} Motores Suzuki Ignis`;
-        } else if (tonelajeTotalKg >= titanes.NIVEL_2.peso_kg) {
-            nivelAlcanzado = titanes.NIVEL_2.nombre;
-            comparativaReal = `${(tonelajeTotalKg / titanes.NIVEL_2.peso_kg).toFixed(1)} Refrigeradores`;
+        if (tonelajeTotalKg >= titanes.NIVEL_3.peso_kg) {
+            return { nivel: titanes.NIVEL_3.nombre, descripcion: `${(tonelajeTotalKg / SAB_CONSTANTS.REALIDAD.IGNIS_4CYL_KG).toFixed(1)} Motores Suzuki Ignis` };
         } else if (tonelajeTotalKg >= titanes.NIVEL_1.peso_kg) {
-            nivelAlcanzado = titanes.NIVEL_1.nombre;
-            comparativaReal = `${(tonelajeTotalKg / SAB_CONSTANTS.REALIDAD.CEMENTO_KG).toFixed(1)} Bultos de Cemento`;
-        } else {
-            comparativaReal = `${(tonelajeTotalKg / SAB_CONSTANTS.REALIDAD.STORMY_KG).toFixed(1)} Unidades Domésticas (Stormy)`;
+            return { nivel: titanes.NIVEL_1.nombre, descripcion: `${(tonelajeTotalKg / SAB_CONSTANTS.REALIDAD.CEMENTO_KG).toFixed(1)} Bultos de Cemento` };
         }
-
-        return { nivel: nivelAlcanzado, descripcion: comparativaReal };
+        return { nivel: "Ninguno", descripcion: `${(tonelajeTotalKg / SAB_CONSTANTS.REALIDAD.STORMY_KG).toFixed(1)} Unidades Domésticas (Stormy)` };
     }
 };
